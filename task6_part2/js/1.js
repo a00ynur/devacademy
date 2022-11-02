f8a1214e0d052fa0d90f8fa2ad9e698eff02f0c0
a00ynur <aynursafarova02@gmail.com>
2022-11-02 12:00:11 +0400

    task

---
+++ task6_part2/js/1.js
@@ -0,0 +1,6 @@
+let A = prompt("Enter the number: ");
+for (let n = 0; n < 1000; n++) {
+   if (n%A == 0) {
+       console.log(n);
+   }   
+}
---
+++ task6_part2/js/2.js
@@ -0,0 +1,6 @@
+let A = prompt("Enter the number: ");
+for (let n = 0; n < A; n++) {
+   if (n*n < A) {
+       console.log(n);
+   }
+}
